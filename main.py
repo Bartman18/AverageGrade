d606432fc0725aed1146af84da60e173380c904e
Bartman18 <125701964+Bartman18@users.noreply.github.com>
2025-02-11 22:43:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from pdf_reader import pdf_reader
 from weighted_average import calculate_weighted_average
 
-pdf_path = "C:/Users/User/PycharmProjects/pdf_Reader/Karta_przebiegu_studiów-273163-2025-02-10_15_51.pdf"
+pdf_path = "your_pdf_path"
 if not 'pdf_output.csv' in os.listdir():
     pdf_reader(pdf_path)
 calculate_weighted_average()
